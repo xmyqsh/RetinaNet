b0b6f853d21ff49c214403e54ebc054891c3600f
xmyqsh <xmyqsh@gmail.com>
2017-09-08 10:03:03 +0800

    delete all

--- lib/networks/__init__.py
+++
@@ -1,9 +0,0 @@
-# --------------------------------------------------------
-# Fast R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
-# --------------------------------------------------------
-
-from .RetinaNet_train_test import RetinaNet_train_test
-from . import factory
